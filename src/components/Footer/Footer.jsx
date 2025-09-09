83e640308bc6b2328e9d41d0e828e9b2757bfed1
zack-das <izackdas@gmail.com>
2025-09-09 20:04:31 +0300

    Update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css';
+
+ function Footer() {
+    
+
+    return (
+        <>
+            <footer className="footer">
+                <p>Designed and built by Isack Das &copy;2025</p>
+            </footer> 
+        </>
+    )
+}
+
+export default Footer;
