9eb8114e3b4cefe9973c850d35a1dcd2adfa2c3a
zack-das <izackdas@gmail.com>
2025-10-31 00:23:10 +0300

    Updates

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { FaGithub } from 'react-icons/fa';
 import './Projects.css';
 import shoezam from '../../assets/shoe-zam.png'
+import  dev from  '../../assets/devs.png'
+import gym from  '../../assets/gym.png'
+
 
 function Projects() {
     
@@ -9,23 +12,23 @@ function Projects() {
         {
             title: "ShoeZam",
             description: "An online shoe store. Frontend built with HTML ,CSS and JavaScript. Backend built with Node.js, Express, PostgreSQL and Sequelize. Features include user authentication, product browsing, shopping cart, and order management.",
-            link:"#",
-            livedemo: "#",
+            link:"https://github.com/zack-das/ShoeZam-Ecommerce-website",
+            livedemo: "shoe-zam-ecommerce-website.vercel.app",
             image: shoezam,
         },
         {
             title: "xDevs-code-hub",
             description: "A platform for developers to connect,share ideas, and publish their articles-from coding tutorials and project write-ups to industry insights. Built with React for the frontend and Node.js with Express for the backend.",
-            link:"#",
-            livedemo: "#",
-            image: "project2.png",
+            link:"https://github.com/zack-das/Xdevs-hub",
+            livedemo: " https://xdevs-hub-1.onrender.com/",
+            image: dev,
         },
         {
-            title: "Project 3",
-            description: "Description of project 3.",
-            link:"#",
-            livedemo: "#",
-            image: "project3.png",
+            title: "GYM and Fitness",
+            description: "I designed and developed the front-end for Gym Yetu Fitness, a modern and responsive website for a fictional fitness center. The goal was to create a compelling online presence that effectively converts visitors into members by showcasing the gym's services, team, and community vibe.",
+            link:"https://github.com/zack-das/GYM_Yetu_Fitness",
+            livedemo: "gym-yetu-fitness.vercel.app",
+            image: gym,
         }
     ]
 
