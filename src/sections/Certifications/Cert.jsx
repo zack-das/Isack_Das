68fa39ca47779c2900b8d2914f8f37d43d225111
zack-das <izackdas@gmail.com>
2026-01-08 22:32:16 +0300

    Updates

--- src/sections/Certifications/Cert.jsx
+++ src/sections/Certifications/Cert.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import './Cert.css';
+import React from 'react';
+import './Cert.css'
+import certificateImage from "../../assets/Responnsivewebdesigncertificate.png";
 
-
-function Cert(){
-	return(
-			<>
-				<div className="certsContainer">
-					
-				</div>
-			</>
-
-		)
-
-};
+function Cert() {
+  return (
+    <>
+    	<div className="cardsOverlay"></div>
+    	 <div className="certs-container">
+    	 	<div className="cert">
+    	 		<a href="https://www.freecodecamp.org/certification/techiedas/responsive-web-design-v9"><img src={certificateImage} alt="CertificateImage"/></a>
+    	 	</div>
+    	 </div>
+    </>
+  );
+}
 
 export default Cert;
